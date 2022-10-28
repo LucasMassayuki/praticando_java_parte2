6af51cd0b1f3da05abf1f358ae072c420228bbc0
Lucas <lucasofficialcruz95@gmail.com>
2022-10-28 15:35:34 -0300

    Informações do caminho do arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,26 +8,14 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter a folder path: ");
+		System.out.println("Enter a file path: ");
 		String strPath = sc.nextLine();
 		
 		File path = new File(strPath);
-		//vai imprimir as pastas 
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS: ");
-		for (File folder : folders){
-			System.out.println(folder);
-		}
-		//vai imprimir os arquivos
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for(File file : files) {
-			System.out.println(file);
-		}
-		//Cria sub pasta 
-		boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created successfully: " + success);
 		
+		System.out.println("getName: " + path.getName());//pego só o nome do arquivo
+		System.out.println("getParent: " + path.getParent());//pasta 
+		System.out.println("getPath: " + path.getPath());// e o caminho completo 
 		sc.close();
 	}
 }
