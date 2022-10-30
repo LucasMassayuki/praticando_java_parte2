c4bd99d5ce88c4cb2b7e339496430dbe5584969f
Lucas <lucasofficialcruz95@gmail.com>
2022-10-30 14:30:14 -0300

    Exercício Proposto

---
+++ src/application/Exercicio_Proposto.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Exercicio_Proposto {
+	/*Fazer um programa para ler o caminho de um arquivo .csv
+	contendo os dados de itens vendidos. Cada item possui um nome, 
+	preço unitário e quantidade, separados por vírgula.
+	Você deve gerar um novo arquivo chamado "summary.csv",
+	localizado em uma subpasta chamada "out" a partit da pasta
+	original do arquivo de origem, contendo apenas o nome e o valor total 
+	parta aquele item (preço unitário multiplicado pela quantidade)*/
+	 
+}
